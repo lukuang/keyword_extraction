5858109d78165df41ce9f5fd8ee2f2f54d0fa0ae
lukuang <lukuang1989@gmail.com>
2016-02-27 11:26:31 -0500

    extract text from html

---
+++ Html_parser.py
@@ -0,0 +1,34 @@
+from goose import Goose, Configuration
+from myStemmer import pstem as stem
+import lxml
+
+
+class Html_parser(object):
+    """
+    use goose to parse raw html and
+    """
+    def __init__(self,need_stem):
+        #set up goose
+        config = Configuration()
+        config.enable_image_fetching = False
+        self._g = Goose(config)
+        self._need_stem = need_stem
+
+    def get_text(self,file_path):
+        raw_html = ""
+        with open(file_path) as f:
+            raw_html = f.read()
+
+        if not raw_html:
+            return None
+        try:
+            article = self._g.extract(raw_html = raw_html)
+        except lxml.etree.ParserError as e:
+            return None
+
+        text = article.title + "\n" + article.cleaned_text 
+        if self._need_stem:
+            words = re.findall("\w+",text.lower(),re.MULTILINE)
+            w = map(stem,words)
+            text = " ".join(w)
+        return text
---
+++ clean_html.py
@@ -0,0 +1,55 @@
+"""
+extract text from html files
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+from Html_parser import Html_parser
+
+
+
+
+def check_top(file_name, top):
+    m=re.match("^(\d+)_\d+$")
+    if m is not None:
+        page = int(m.group(1))
+        if page <= top:
+            return True
+    return False
+
+
+def get_files(a_dir,top):
+    all_files = os.walk(a_dir).next()[2]
+    files = []
+    for f in all_files:
+        if check_top(f,top):
+            files.append(f)
+    return files
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("source_dir")
+    parser.add_argument("dest_dir")
+    #parser.add_argument("phrase_dir")
+    parser.add_argument("--need_stem","-m",action='store_true',default=False)
+    parser.add_argument("--top","-t",type=int,default=10)
+    args=parser.parse_args()
+
+    parser = Html_parser(args.need_stem)
+
+    files = get_files(args.source_dir,args.top)
+    for f in files:
+        dest_file = os.path.join(args.dest_dir,f)
+        text = parser.get_text(os.path.join(args.source_dir,f))
+        with open(dest_file,"w") as of:
+            of.write(text)
+
+
+
+if __name__=="__main__":
+    main()
+
