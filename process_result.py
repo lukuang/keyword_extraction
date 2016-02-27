21436be56c876b82ca7461b8a977294af3956a02
lukuang <lukuang1989@gmail.com>
2016-02-27 17:59:22 -0500

    process result from standford NLP

--- process_result.py
+++ process_result.py
@@ -8,11 +8,33 @@ import sys
 import re
 import argparse
 
+
+def get_json(source):
+    data = {}
+    tag = ""
+    with open(source,"r") as f:
+        for line in f:
+            line = line.rstrip()
+            m = re.search("^(\w+):$")
+            if m is not None:
+                tag = m.group(1)
+                data[tag] = {}
+            else:
+                m = re.search("^(\w+):(\d+)$")
+                if m is not None:
+                    data[tag][m.group(1)] = int(m.group(2))
+            else:
+                print "line did not match:"
+                print line
+    with open(source+".json","w") as f:
+        f.write(json.dumps(data))
+
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("source")
     parser.add_argument("dest")
     args=parser.parse_args()
+
 
 if __name__=="__main__":
     main()
