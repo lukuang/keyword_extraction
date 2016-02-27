f06aa7482c04d2dadb999540f048ea9d5d774cd8
lukuang <lukuang1989@gmail.com>
2016-02-27 18:18:34 -0500

    fix bug

--- process_result.py
+++ process_result.py
@@ -8,8 +8,8 @@ import sys
 import re
 import argparse
 import codecs
-reload(sys)
-sys.setdefaultencoding('UTF8')
+#reload(sys)
+#sys.setdefaultencoding('UTF8')
 
 
 def get_json(source):
@@ -41,6 +41,7 @@ def output_top(data,dest,top):
         sorted_sub = sorted(data[tag].items(),key = lambda x:x[1], reverse=True)
         f.write("%s:\n" %tag)
         for (k,v) in sorted_sub:
+            print k
             f.write("\t"+k+":"+str(v)+"\n")
         for p in data[tag]:
             if p not in all_phrases:
