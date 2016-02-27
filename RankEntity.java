7a476a9e610a3be5994a62845b106abdd1dad1d8
lukuang <lukuang1989@gmail.com>
2016-02-27 15:12:42 -0500

    fix bug

--- RankEntity.java
+++ RankEntity.java
@@ -1,11 +1,11 @@
-import edu.stanford.nlp.ie.AbstractSequenceClassifier;
-import edu.stanford.nlp.ie.crf.*;
-import edu.stanford.nlp.io.IOUtils;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
-import edu.stanford.nlp.util.Triple;
-
+// import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+// import edu.stanford.nlp.ie.crf.*;
+// import edu.stanford.nlp.io.IOUtils;
+// import edu.stanford.nlp.ling.CoreLabel;
+// import edu.stanford.nlp.ling.CoreAnnotations;
+// import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
+// import edu.stanford.nlp.util.Triple;
+import java.nio.file;
 import java.util.List;
 
 public class RankEntity {
@@ -15,7 +15,7 @@ public class RankEntity {
     String serializedClassifier = "/home/1546/source/stanford-ner-2015-12-09/classifiers/english.all.3class.distsim.crf.ser.gz";
 
 
-    AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+    //AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
 
     /* For either a file to annotate or for the hardcoded text example, this
        demo file shows several ways to process the input, for teaching purposes.
