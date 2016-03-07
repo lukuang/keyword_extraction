b0606c2a33585fef50cbb63aa58fc8a1ecc35621
lukuang <lukuang1989@gmail.com>
2016-03-07 17:24:24 -0500

    change run.py

--- run.py
+++ run.py
@@ -9,27 +9,22 @@ import re
 import argparse
 import subprocess
 
-def main():
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("source_dir")
-    parser.add_argument("dest_dir")
-    parser.add_argument("entity_dir")
-    parser.add_argument("--src_dir","-s", default="/lustre/scratch/lukuang/keyphrase_extraction/src")
-    parser.add_argument("--top",'-t',type=int,default=10)
-    args=parser.parse_args()
-    
+
+def call_clean_html(source_dir, dest_dir, top):
     args_clean_html = [
         'python',
         '/lustre/scratch/lukuang/keyphrase_extraction/src/clean_html.py',
-        args.source_dir,
-        args.dest_dir,
+        source_dir,
+        dest_dir,
         "-t",
-        str(args.top),
+        str(top),
         '-o'
     ]
 
     subprocess.call(args_clean_html)
 
+
+def gene_entities(dest_dir,entity_dir):
 
     java_files = {
         "RankEntity" : "tf",
@@ -37,14 +32,12 @@ def main():
         "RankEntityDFDiscounted" : "dfd"
     }
 
-    os.chdir(args.src_dir)
-    print os.getcwd()
     args_run_java = [
         'java',
         '-cp',
         "'.:/home/1546/source/stanford-ner-2015-12-09/*:/home/1546/source/stanford-ner-2015-12-09/lib/*'",
         "JAVA FILE",
-        args.dest_dir,
+        dest_dir,
     ]
 
     args_process_result = [
@@ -59,12 +52,29 @@ def main():
         print " ".join(args_run_java)
         p = subprocess.Popen(" ".join(args_run_java), stdout=subprocess.PIPE, shell=True)
         output = p.communicate()[0]
-        file_name = os.path.join(args.entity_dir,java_files[java_file]+"_all_entity")
+        file_name = os.path.join(entity_dir,java_files[java_file]+"_all_entity")
         with open(file_name,'w') as f:
             f.write(output)
         args_process_result[2] = file_name
-        args_process_result[3] = os.path.join(args.entity_dir,java_files[java_file])
+        args_process_result[3] = os.path.join(entity_dir,java_files[java_file])
         subprocess.call(args_process_result)
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("source_dir")
+    parser.add_argument("dest_dir")
+    parser.add_argument("entity_dir")
+    parser.add_argument("--src_dir","-s", default="/lustre/scratch/lukuang/keyphrase_extraction/src")
+    parser.add_argument("--top",'-t',type=int,default=10)
+    args=parser.parse_args()
+    
+    call_clean_html(args.source_dir, args.dest_dir, args.top)
+
+    os.chdir(args.src_dir)
+    gene_entities(args.dest_dir,args.entity_dir)
+    
+
+
 
 
     
