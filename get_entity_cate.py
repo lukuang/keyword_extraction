b3cf1af7949238b68c72aa83168f8157a77e84b3
lukuang <lukuang1989@gmail.com>
2016-03-08 10:31:56 -0500

    add get entity category information

---
+++ get_entity_cate.py
@@ -0,0 +1,32 @@
+"""
+get category information for an entity
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+from myUtility.wikiapi import *
+
+
+def get_entity_cate(entity):
+    wikipedia_caller = Wikipedia()
+    wikidata_caller = Wikidata()
+    entity_name = wikipedia_caller.get_entity_name(entity)
+    entity_info = wikidata_caller.get_entity_info_by_name(entity_name)
+    return {entiy: entity_info['class_info']}
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("entity")
+    args=parser.parse_args()
+    entity_cate = get_entity_cate(args.entity)
+    print entity_cate
+    
+
+
+
+if __name__=="__main__":
+    main()
+
