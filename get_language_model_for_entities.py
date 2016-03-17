b74e861b68758ac74e3bd38590d63d8bf1fa6d47
lukuang <lukuang1989@gmail.com>
2016-03-17 09:59:10 -0400

    fix bug

--- get_language_model_for_entities.py
+++ get_language_model_for_entities.py
@@ -97,7 +97,7 @@ def main():
         documents[instance] = []
         for a_dir in sub_dirs:
             date_dir = os.path.join(source_dir,a_dir)
-            for single_file in get_files():
+            for single_file in get_files(date_dir):
                 documents[instance][single_file] = Document(single_file,file_path = os.path.join(date_dir,f))
 
     show_documents(documents)#debug purpose
