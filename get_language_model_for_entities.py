0fb047cb2245914ee78a791abc689697a0197076
lukuang <lukuang1989@gmail.com>
2016-03-17 10:35:15 -0400

    get window for each entity

--- get_language_model_for_entities.py
+++ get_language_model_for_entities.py
@@ -18,8 +18,8 @@ def get_sentence_window(words,sentence,windows):
         if w not in windows:
             windows[w] = []
         if sentence.find(w) != -1:
-            window_string.replace(w,"")
-        windows[w].append(Sentence(window_string,remove_stopwords=True))
+            sentence.replace(w,"")
+        windows[w].append(Sentence(sentence,remove_stopwords=True))
 
 
 
@@ -109,7 +109,7 @@ def main():
             date_dir = os.path.join(source_dir,a_dir)
             print date_dir
             for single_file in get_files(date_dir):
-                print "open file %s" %os.path.join(date_dir,single_file)
+                #print "open file %s" %os.path.join(date_dir,single_file)
                 documents[instance][single_file] = Document(single_file,file_path = os.path.join(date_dir,single_file))
 
     #show_documents(documents)#debug purpose
