917e0fb5f7271b4060cf1f5543113f111cf46f57
lukuang <lukuang1989@gmail.com>
2016-04-05 11:08:34 -0400

    debug

--- get_language_model_for_negative_entities.py
+++ get_language_model_for_negative_entities.py
@@ -186,8 +186,6 @@ def get_all_sentence_windows(documents,entities_judgement,negative_candidates):
 def get_all_text_windows(documents,entities_judgement,negative_candidates,window_size):
     windows = {}
     for instance in documents:
-        if instance!='2012_oklahoma_kansas_tornado':
-            continue
         print "%s:" %instance
         words = set()
         windows[instance] = {}
