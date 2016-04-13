c2b9b48169135fb2954d61ca4fab4bb4f90224d2
lukuang <lukuang1989@gmail.com>
2016-04-13 10:51:11 -0400

    exclude no news episode and no-macthing-original episodes

--- match_entities.py
+++ match_entities.py
@@ -92,7 +92,7 @@ def match_entities(narrative_entities,original_entities,news_entities):
             elif entity in original_entities[eid]:
                 original_match += 1 
         single_match = narrative_match + original_match
-        matched += single_match
+        
 
         
 
@@ -104,17 +104,19 @@ def match_entities(narrative_entities,original_entities,news_entities):
             if (original_match==0):
                 no_match_original.append(eid)
                 continue
-        if len(narrative_entities[eid]) == 0:
-            match_percent_narrative.append(.0)
 
+        matched += single_match
         total_narrative += len(narrative_entities[eid])
         total_original += len(original_entities[eid])
         sinlge_episode_entities = narrative_entities[eid] + original_entities[eid]
         total_episode += len(sinlge_episode_entities)
         total_news += len(news_entities[eid])
-        
+
+        if len(narrative_entities[eid]) == 0:
+            match_percent_narrative.append(.0)
         else:
             match_percent_narrative.append((narrative_match*1.0)/len(narrative_entities[eid]))
+
         match_percent_original.append((original_match*1.0)/len(original_entities[eid]))
         match_percent_episode.append((single_match*1.0)/len(sinlge_episode_entities))
         match_percent_news.append((single_match*1.0)/len(news_entities[eid]))
