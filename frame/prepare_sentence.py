2fb6317f854e42c541f4cfeed04817e6ee2335b9
lukuang <lukuang1989@gmail.com>
2016-04-18 17:45:25 -0400

    debug

--- frame/prepare_sentence.py
+++ frame/prepare_sentence.py
@@ -60,7 +60,7 @@ def get_sentence_window(entity_map,sentence,windows):
             if w not in windows:
                 windows[w] = []
             
-            windows[w].append(sentence)
+            windows[w].append(re.sub(sentence,"\n"," "))
 
 
 
