7ed9fcdfaad60d4c8c4e82ee515e9ff51b02e656
lukuang <lukuang1989@gmail.com>
2016-04-21 14:52:45 -0400

    debug

--- stanford_parser/VerbPairFinder.java
+++ stanford_parser/VerbPairFinder.java
@@ -74,7 +74,14 @@ class VerbPairFinder {
 
   public static JSONObject read_entity_file(String file_path){
     JSONParser parser = new JSONParser();
-    String content = new Scanner(new File(file_path)).useDelimiter("\\Z").next();
+    try{
+      String content = new Scanner(new File(file_path)).useDelimiter("\\Z").next();
+    }catch(FileNotFoundException fe){
+        System.out.println("Cannot find file "+file_name);
+        System.out.println(fe);
+
+    }
+    
     try{
       Object obj = parser.parse(content);
       JSONObject loaded_obj = (JSONObject) obj;
