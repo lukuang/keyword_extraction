07fe2ff9a5886533e8af66f894e3040ba342a5ba
lukuang <lukuang1989@gmail.com>
2016-04-21 14:32:48 -0400

    add code to generate verb list for every sentence

---
+++ stanford_parser/VerbPairFinder.java
@@ -0,0 +1,159 @@
+
+import java.util.Collection;
+import java.util.List;
+import java.io.*;
+
+import edu.stanford.nlp.process.Tokenizer;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+
+class VerbPairFinder {
+
+  /**
+   * The main method demonstrates the easiest way to load a parser.
+   */
+  public static void main(String[] args) {
+
+
+    if (args.length == 2) {
+      String parserModel = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
+
+      LexicalizedParser lp = LexicalizedParser.loadModel(parserModel);
+      List <String> content = read_file(args[0]);
+      JSONObject entity_content = read_entity_file(args[1]);
+      JSONObject result= new JSONObject();
+      for (int i=0;i<content.size();i++){
+        String index = (i+1).toString();
+        JSONObject sub_data = entity_content.get(index);
+        String entity = sub_data.get('entity');
+        List <String> sentence_verbs = find_verb_pair_in_sentence(lp,entity,content.get(i));
+        JSONArray verb_array = new JSONArray();
+        for(int j=0;j<sentence_verbs.size();j++){
+          verb_array.add(sentence_verbs.get(i));
+        }
+        sub_data.put("verbs",verb_array);
+        result.put(index,sub_data);
+      }
+    } else {
+      System.out.println("ERROR: use file_name and entity file as input!");
+    }
+  }
+
+  public List <String> read_file(String file_name){
+    try (BufferedReader br = new BufferedReader(new FileReader(file_name))) {
+      String line;
+      List <String> content = new List<String>();
+      while ((line = br.readLine()) != null) {
+       // process the line.
+        content.add(line);
+
+      }
+      return content;
+    }
+    catch(FileNotFoundException fe){
+        System.out.println("Cannot find file "+file_name);
+        System.out.println(fe);
+
+    } 
+
+  }
+
+
+  public JSONParser read_entity_file(String file_path){
+    JSONParser parser = new JSONParser();
+        String content = new Scanner(new File(file_path)).useDelimiter("\\Z").next();
+        obj = parser.parse(content);
+        loaded_obj = (JSONObject) obj;
+        return loaded_obj;
+        //System.out.println("Loaded size: "+ loaded_obj.size());
+        Iterator<?> eid = loaded_obj.keySet().iterator();
+        while(eid.hasNext()){
+            String key = (String)eid.next();
+            if(key=="69591"){
+                System.out.println("found 69591!");
+            }
+            JSONObject sub_data = (JSONObject) loaded_obj.get(key);
+            narrative_map.put(key,(String)sub_data.get("narrative"));
+            JSONArray original_entities = (JSONArray) sub_data.get("entities");
+            for (int i=0;i<original_entities.size();i++){
+                String name = (String)original_entities.get(i);
+                original_entitiy_map.doublePut(key,name);
+            }
+
+        }
+  }
+
+
+  /**
+   * find_verb_pair_in_sentence takes a sentence and an entity as input,
+   * and return the verb-entity back 
+   */
+  public static void find_verb_pair_in_sentence(LexicalizedParser lp, String entity, String sentence) {
+    // This option shows parsing a list of correctly tokenized words
+    
+
+
+    // This option shows loading and using an explicit tokenizer
+    TokenizerFactory<CoreLabel> tokenizerFactory =
+        PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
+    Tokenizer<CoreLabel> tok =
+        tokenizerFactory.getTokenizer(new StringReader(sentence));
+    List<CoreLabel> rawWords2 = tok.tokenize();
+    parse = lp.apply(rawWords2);
+
+    TreebankLanguagePack tlp = lp.treebankLanguagePack(); // PennTreebankLanguagePack for English
+    GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+    GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+    List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
+
+    List <String> verbs = new List<String>();
+    for (int i=0;i<tdl.size();i++){
+        System.out.println(tdl.get(i).reln().getShortName());
+  
+        String rel = tdl.get(i).reln().getShortName().toString();
+        if (rel.equals("root")){
+		      continue;
+        }
+        String d_tag = tdl.get(i).dep().tag();
+        String g_tag = tdl.get(i).gov().tag();
+        String d_word = tdl.get(i).dep().word();
+        String g_word = tdl.get(i).gov().word();
+        if(g_word.equals(entity) ){
+            if (d_tag.contains("VB") ){
+                //System.out.println("Found!");
+                //System.out.println(d_word+" "+g_word+" "+rel);
+                verbs.add(d_word);
+            } 
+        }
+        else if(d_word.equals(entity) ){
+            if (g_tag.contains("VB") ){
+                //System.out.println("Found!");
+                //System.out.println(d_word+" "+g_word+" "+rel);
+                verbs.add(g_word);
+            }
+        }
+
+        //System.out.println(word+" / "+tag);
+        //System.out.println(tdl.get(i).gov());
+    }
+    return verbs;
+    // You can also use a TreePrint object to print trees and dependencies
+    //TreePrint tp = new TreePrint("penn,typedDependenciesCollapsed");
+    //tp.printTree(parse);
+  }
+
+  private VerbPairFinder() {} // static methods only
+
+}
