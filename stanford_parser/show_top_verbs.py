5655c9825591f8ee371a14a8fb56b6eefdfd889c
lukuang <lukuang1989@gmail.com>
2016-04-27 11:12:29 -0400

    add utility to show top words

--- stanford_parser/show_top_verbs.py
+++ stanford_parser/show_top_verbs.py
@@ -29,7 +29,7 @@ def remap_tuples(tuple_file):
 def get_verbs(tuple_result):
     verbs = {}
     for identifier in tuple_result:
-        verb_model = Model(True)
+        verb_model = Model(True,need_stem=True)
         for single_tuple in tuple_result[identifier]:
             verb = single_tuple['verb']
             if verb not in NO_NEED:
@@ -47,7 +47,7 @@ def get_all_words(tuple_results):
     for identifier in tuple_results:
         word_model = Model(True)
         for single_tuple in tuple_results[identifier]:
-            word_model += Sentence(single_tuple['sentence'],remove_stopwords=True,need_stem=True).stemmed_model
+            word_model += Sentence(single_tuple['sentence'],remove_stopwords=True).stemmed_model
 
         word_model.normalize()
         for word in word_model.model:
