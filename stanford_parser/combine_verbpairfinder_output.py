244a63952ea6f3fe752c9ebe3f00e9ccb3b0872c
lukuang <lukuang1989@gmail.com>
2016-04-27 09:39:41 -0400

    add code to combine the sub results of VerbPairFinder

---
+++ stanford_parser/combine_verbpairfinder_output.py
@@ -0,0 +1,33 @@
+"""
+combine the output results of VerbPairFinder
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+import codecs
+
+def combine_verbpairfinder_output(s_dir):
+    data = {}
+    for f in os.walk(s_dir).next()[2]:
+        f = os.path.join(s_dir,f)
+        data.update(json.load(open(f)))
+    return data
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("s_dir")
+    parser.add_argument("dest_file")
+
+    args=parser.parse_args()
+    data = combine_verbpairfinder_output(args.s_dir)
+    with open(args.dest_file,'w','utf-8') as f:
+        f.write(data)
+
+
+if __name__=="__main__":
+    main()
+
