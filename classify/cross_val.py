52f0613c724820ae9d83e258dac2f642ce4ccfd5
lukuang <lukuang1989@gmail.com>
2016-05-11 09:19:28 -0400

    show also the preformance associated with entity types

--- classify/cross_val.py
+++ classify/cross_val.py
@@ -9,6 +9,7 @@ import re
 import argparse
 from sklearn import cross_validation
 from sklearn import metrics
+from collections import Counter,namedtuple
 
 METHOD = ['linear_svc','logistic_regression',"naive_bayes"]
 
@@ -17,8 +18,9 @@ def load_data_set(data_dir):
     features = json.load(open(os.path.join(data_dir,"feature_vector")))
     judgements = json.load(open(os.path.join(data_dir,"judgement_vector")))
     all_entities = json.load(open(os.path.join(data_dir,"all_entities")))
+    all_type_mapping = json.load(open(os.path.join(data_dir,"all_type_mapping")))
 
-    return features,judgements,all_entities
+    return features,judgements,all_entities,all_type_mapping
 
 
 
@@ -63,6 +65,36 @@ def get_top_entities(all_entities,predicted,top_size,need_positive):
         print "\t%s: %d" %(sorted_entities[i][0],sorted_entities[i][1])
 
 
+def show_performance_on_entity_types(y,predicted,all_type_mapping):
+    Size = namedtuple('Size', ['pos', 'neg'])
+    type_size = Size(pos=Counter(),neg=Counter())
+    correct_predicted = Size(pos=Counter(),neg=Counter())
+    for i in range(len(all_type_mapping)):
+        entity_type_list = all_type_mapping[i]
+        if y[i] == 1:
+            type_size.pos.update(entity_type_list)
+            if predicted[i] == 1:
+                correct_predicted.pos.update(entity_type_list)
+
+        else:
+            type_size.neg.update(entity_type_list)
+            if predicted[i] == 0:
+                correct_predicted.neg.update(entity_type_list)
+
+    print "show performance in terms of entity types:"
+    print "There are %d entities"
+    print "In positive example:"
+    for k in type_size.pos.keys():
+        print "\tThere are %d %s entities, %d are predicted correctly"\
+             %(type_size.pos[k],k,correct_predicted.pos[k])
+    print "In negative example:"
+    for k in type_size.neg.keys():
+        print "\tThere are %d %s entities, %d are predicted correctly"\
+             %(type_size.neg[k],k,correct_predicted.neg[k])
+
+
+
+
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--data_dir","-dr",default = "/home/1546/code/keyword_extraction/stanford_parser/no_location_features")
@@ -78,15 +110,16 @@ def main():
     parser.add_argument("--need_positive","-np",action='store_true')
 
     args=parser.parse_args()
-    X,y,all_entities = load_data_set(args.data_dir)
+    X,y,all_entities,all_type_mapping = load_data_set(args.data_dir)
     clf = get_classifier(args.method)
     predicted = cross_validation.cross_val_predict(clf,X,y,cv=5)
     accuracy = metrics.accuracy_score(y,predicted)
     f1 = metrics.f1_score(y,predicted)
+
     print "performance:"
     print "accuracy: %f, f1: %f" %(accuracy,f1)
-
-    get_top_entities(all_entities,predicted,args.top_size,args.need_positive)
+    show_performance_on_entity_types(y,predicted,all_type_mapping)
+    #get_top_entities(all_entities,predicted,args.top_size,args.need_positive)
 
 if __name__=="__main__":
     main()
