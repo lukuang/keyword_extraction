95df89771eb937e4b8128df71bd8e6ea2876622d
lukuang <lukuang1989@gmail.com>
2016-05-26 10:26:02 -0400

    change generate verb features for new_tornado

--- classify/gene_features_for_verbs.py
+++ classify/gene_features_for_verbs.py
@@ -335,7 +335,7 @@ def main():
     all_word_features = set()
     entities = set()
     entity_type_mapping = get_entity_type_mapping(args.news_entity_dir,args.required_entity_types,args.required_file_name)
-    print entity_type_mapping
+    #print entity_type_mapping
     if not args.new_tornado:
         year_mapping = get_year_mapping(args.query_file)
     else:
@@ -343,7 +343,7 @@ def main():
         for instance in entity_type_mapping:
             year_mapping[unicode(instance)] = "2012"
 
-    print year_mapping
+    #print year_mapping
 
     negative_word_features,negative_entities,negative_features =\
             process_result_tuple(args.negative_file,args.word_feature_size,args.use_clause_words)
