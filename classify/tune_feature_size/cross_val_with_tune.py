6a5386f6895c68545e135786955e088a461db2ab
Kuang Lu <lukuang@n028.farber.hpc.udel.edu>
2016-09-27 10:00:58 -0400

    tune para in each set(probably problematic)

--- classify/tune_feature_size/cross_val_with_tune.py
+++ classify/tune_feature_size/cross_val_with_tune.py
@@ -8,17 +8,20 @@ import sys
 import re
 import argparse
 from sklearn import cross_validation
+import sklearn.metrics
 from sklearn.metrics import classification_report
 from sklearn import metrics
 from collections import Counter,namedtuple
+import numpy as np
 
 METHOD = ['linear_svc','logistic_regression',"naive_bayes"]
 
+MaxPara = namedtuple('MaxPara', ['ws_input', 'cs_input', 'ws_real', 'cs_real', 'max_f1',"sub_predicted_y"])
 
 def load_data_set(data_dir):
     feature_data = json.load(open(os.path.join(data_dir,"feature_data")))
 
-    return features,judgements,feature_data
+    return feature_data
 
 
 
@@ -84,41 +87,56 @@ def get_limit(negative_frequency,positive_frequency):
     return limit
 
 
-def prepare_data(train_data,what_to_tune):
+def prepare_data(train_data,test_data,what_to_tune):
     negative_feature_frequency = {}
     positive_feature_frequency = {}
     negative_category_frequency = {}
     positive_category_frequency = {}
+    train_judgement_vector = []
+    test_judgement_vector = []
+
+
 
     for single_data in train_data:
         if single_data["judgement"] == 0:
-                for w in single_data["word_features"]: 
-                    if w not in negative_feature_frequency:
-                        negative_feature_frequency[w] = 0
-                    negative_feature_frequency[w] += 1
+            train_judgement_vector.append(0)
+            for w in single_data["word_features"]: 
+                if w not in negative_feature_frequency:
+                    negative_feature_frequency[w] = 0
+                negative_feature_frequency[w] += 1
 
-                if not single_data["category"]:
-                    continue
+            if not single_data["category"]:
+                continue
 
-                for c in single_data["category"]:
-                    if c not in negative_category_frequency:
-                        negative_category_frequency[c] = 0
-                    negative_category_frequency[c] += 1
+            for c in single_data["category"]:
+                if c not in negative_category_frequency:
+                    negative_category_frequency[c] = 0
+                negative_category_frequency[c] += 1
 
 
-            else:
-                for w in single_data["word_features"]: 
-                    if w not in positive_feature_frequency:
-                        positive_feature_frequency[w] = 0
-                    positive_feature_frequency[w] += 1
+        else:
+            train_judgement_vector.append(1)
+            for w in single_data["word_features"]: 
+                if w not in positive_feature_frequency:
+                    positive_feature_frequency[w] = 0
+                positive_feature_frequency[w] += 1
 
-                if not single_data["category"]:
-                    continue
+            if not single_data["category"]:
+                continue
 
-                for c in single_data["category"]:
-                    if c not in positive_category_frequency:
-                        positive_category_frequency[c] = 0
-                    positive_category_frequency[c] += 1
+            for c in single_data["category"]:
+                if c not in positive_category_frequency:
+                    positive_category_frequency[c] = 0
+                positive_category_frequency[c] += 1
+
+
+    for single_data in test_data:
+        if single_data["judgement"] == 0:
+            test_judgement_vector.append(0)
+        else:
+            test_judgement_vector.append(1)
+
+
 
     ws_limit = get_limit(negative_feature_frequency,positive_feature_frequency)
     cs_limit = get_limit(negative_category_frequency,positive_category_frequency)
@@ -128,21 +146,24 @@ def prepare_data(train_data,what_to_tune):
     sorted_negative_category_frequency = sorted(negative_category_frequency.items(),key = lambda x:x[1],reverse=True)
     sorted_positive_category_frequency = sorted(positive_category_frequency.items(),key = lambda x:x[1],reverse=True)
 
+    print ws_limit,cs_limit
     para_set = []
     if what_to_tune == 0:
-        for ws_input in range(10,ws_limit*10+10):
+        for ws_input in range(10,ws_limit,10):
             para_set.append([ws_input,0])
     elif what_to_tune == 1:
-        for cs_input in range(10,cs_input*10+10):
+        for cs_input in range(10,cs_limit,10):
             para_set.append([0,cs_input])
 
     else:
-        for ws_input in range(10,ws_limit*10+10):
-            for cs_input in range(10,cs_input*10+10):
+        for ws_input in range(10,ws_limit,10):
+            for cs_input in range(10,cs_limit,10):
                 para_set.append([ws_input,cs_input])
 
-    return para_set,
-           sorted_negative_feature_frequency,sorted_positive_feature_frequency,
+    #print para_set
+    return para_set,\
+           train_judgement_vector,test_judgement_vector,\
+           sorted_negative_feature_frequency,sorted_positive_feature_frequency,\
            sorted_negative_category_frequency,sorted_positive_category_frequency
 
 
@@ -164,23 +185,73 @@ def get_top_common_feature(sorted_positive_feature, sorted_negative_feature,feat
 
     return list(set(top_features))
 
+def get_sub_feature_vector(sub_feature_date,top_context_feature,top_category_feature):
+    sub_feature_vector = []
+    for single_data in sub_feature_date:
+        single_feature_vector = []
+        for w in top_context_feature:
+            if w in single_data["word_features"]:
+                single_feature_vector.append(single_data["word_features"][w])
+            else:
+                single_feature_vector.append(0)
 
-def tune(train_data,test_data,what_to_tune):
+        if not single_data["category"]:
+                single_feature_vector += [0]*len(top_category_feature)
+        else:
+            for c in top_category_feature:
+                if c in single_data["category"]:
+                    single_feature_vector.append(1)
+                else:
+                    single_feature_vector.append(0)
+        # if len(single_feature_vector)==1:
+        #     print single_data
+        #     print single_feature_vector
+        sub_feature_vector.append(single_feature_vector)
+
+    return sub_feature_vector
+
+
+
+def get_feature_vector(train_data,test_data,
+                       top_context_feature,top_category_feature):
+    train_feature_vector =\
+        get_sub_feature_vector(train_data,top_context_feature,top_category_feature)
+    
+    test_feature_vector =\
+        get_sub_feature_vector(test_data,top_context_feature,top_category_feature)
+    
+    return train_feature_vector, test_feature_vector
+
+
+def tune(train_data,test_data,clf,what_to_tune):
     para_set,\
+    train_judgement_vector,test_judgement_vector,\
     sorted_negative_feature_frequency,sorted_positive_feature_frequency,\
     sorted_negative_category_frequency,sorted_positive_category_frequency\
-        = prepare_data(train_data,what_to_tune)
+        = prepare_data(train_data,test_data,what_to_tune)
 
+    max_para = MaxPara._make([0,0,0,0,0,[]])
     for ws_input, cs_input in para_set:
         top_context_feature = get_top_common_feature(sorted_negative_feature_frequency,sorted_positive_feature_frequency,ws_input)
         top_category_feature = get_top_common_feature(sorted_negative_category_frequency,sorted_positive_category_frequency,cs_input)
         ws_real = len(top_context_feature)
         cs_real = len(top_category_feature)
 
-        train_feature_vector = []
-        train_judgement_vector = []
-        test_feature_vector = []
-        test_judgement_vector = []
+        train_feature_vector, test_feature_vector =\
+            get_feature_vector(train_data,test_data,
+                               top_context_feature,top_category_feature)
+
+    
+        clf.fit(np.array(train_feature_vector), np.array(train_judgement_vector)) 
+        temp_predicted_y = clf.predict(test_feature_vector)
+
+        new_f1 = sklearn.metrics.f1_score(test_judgement_vector,temp_predicted_y,average='binary')
+        #print "f1 now %f" %new_f1
+        if new_f1 > max_para.max_f1:
+            max_para = MaxPara._make([ws_input,cs_input,ws_real,cs_real,new_f1,temp_predicted_y])
+
+
+    return max_para
 
         
 
@@ -223,6 +294,7 @@ def main():
         #split the data
         train_index = train_index.tolist()
         test_index = test_index.tolist()
+        print "%d training data and %d test data" %(len(train_index),len(test_index))
         train_data = []
         test_data = []
 
@@ -232,14 +304,14 @@ def main():
         for i in test_index:
             test_data.append(feature_data[ i ])
 
-        sub_predicted_y,ws_input,cs_input, ws_real, cs_real
-                = tune(train_data,test_data,args.what_to_tune)
+        sub_max_para = tune(train_data,test_data,clf,args.what_to_tune)
 
         print "ws_input:%d, cs_input:%d, ws_real:%d, cs_real:%d"\
-            %(ws_input,cs_input, ws_real, cs_real)
+            %(sub_max_para.ws_input, sub_max_para.cs_input, 
+              sub_max_para.ws_real, sub_max_para.cs_real)
 
         for i in range(len(test_index)):
-            predicted[ test_index[i] ] = sub_predicted_y[i]
+            predicted[ test_index[i] ] = sub_max_para.sub_predicted_y[i]
 
 
 
