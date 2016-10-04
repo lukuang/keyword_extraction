e4587b0e06639bd8da05f4d0736c478e097b28ae
Kuang Lu <lukuang@login000.(none)>
2016-10-04 11:04:22 -0400

    only use the sentences that clauses/verbs can be detected from

--- sentence_level/test_effectiveness/gene_features_for_words.py
+++ sentence_level/test_effectiveness/gene_features_for_words.py
@@ -15,13 +15,20 @@ def get_word_features(judged_data_file):
     judged_data = json.load(open(judged_data_file))
     feature_data = []
     for single_data in judged_data:
-        sentence = single_data["sentence"]
-        sentence_model = Sentence(re.sub("\n"," ",sentence),remove_stopwords=False).raw_model
-        sentence_model.to_dirichlet()
+        if "result_tuples" not in single_data:
+            raise RuntimeError("data does not have result_tuple field")
+        else:
+            # only use the sentences that clauses/verbs can be detected
+            # from
+            if single_data["result_tuples"]:
+                sentence = single_data["sentence"]
+                sentence_model = Sentence(re.sub("\n"," ",sentence),remove_stopwords=False).raw_model
+                sentence_model.to_dirichlet()
 
-        single_data.pop("sentence",None)
-        single_data["word_features"] = sentence_model.model
-        feature_data.append(single_data)
+                single_data.pop("sentence",None)
+                single_data["word_features"] = sentence_model.model
+                feature_data.append(single_data)
+
     return feature_data
 
 
