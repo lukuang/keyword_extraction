5fd8a1cd88e0a6200d8dff6bd3a6b8f96c526d23
Kuang Lu <lukuang@g115.farber.hpc.udel.edu>
2016-10-06 16:44:15 -0400

    change the way it reads types

--- sentence_level/prepare_basic.py
+++ sentence_level/prepare_basic.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 use judged data to create basic data used to generate training/testing data
 """
@@ -32,6 +33,33 @@ def get_cate_info(pure_entities,cate_info_file):
             f.write(json.dumps(cate_info)) 
     return cate_info
 
+def get_type_info(candidate_file,required_types):
+    type_info = {}
+
+    for t in required_types:
+        type_info[t] = set()
+
+    with open(candidate_file) as f:
+        for line in f:
+            line = line.rstrip()
+            m = re.search("^([A-Z]+):(.+?):(.+)$",line)
+            #parts = line.split(':')
+
+            try:
+                entity_type = m.group(1)
+            except Exception:
+                print line
+                sys.exit(-1)
+            if entity_type in required_types:
+                entity = unicode(m.group(2))
+                #if entity == u"Kosair Children’s Hospital":
+                #    print "Found sentence %s" %line
+                type_info[entity_type].add(entity)
+
+    return type_info
+
+
+
 
 def get_judged_data(source_file,type_info,cate_info_file):
     """Return judged data for xlsx file as 
@@ -44,14 +72,19 @@ def get_judged_data(source_file,type_info,cate_info_file):
     ws = wb.get_sheet_by_name('Sheet1')
     for row in ws.iter_rows():
         instance = row[0].value
-        entity = row[1].value
+        entity = unicode(row[1].value)
         sentence = row[2].value
         judgement = row[3].value
 
         types = []
+        found_type = False
         for entity_type in type_info:
             if entity in type_info[entity_type]:
                 types.append(entity_type)
+                found_type = True
+        if not found_type:
+            print "no type for %s" %(entity)
+            sys.exit(-1)
 
         entities.add(entity)
         single_data = {
@@ -78,12 +111,13 @@ def get_judged_data(source_file,type_info,cate_info_file):
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("source_file")
-    parser.add_argument("type_file")
+    parser.add_argument("candidate_file")
     parser.add_argument("dest_file")
+    parser.add_argument("--required_types", "-rt",nargs='+',default=["ORGANIZATION","LOCATION"])
     parser.add_argument("--cate_info_file","-cf",default="/home/1546/code/keyword_extraction/sentence_level/data/other_data/cate_info.json")
     args=parser.parse_args()
 
-    type_info = json.load(open(args.type_file))
+    type_info = get_type_info(args.candidate_file,args.required_types)
 
     judged_data = get_judged_data(args.source_file,type_info,args.cate_info_file)
 
