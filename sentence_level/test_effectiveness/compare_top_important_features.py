7336b6c741c276d3a1aeddf3107811c56d2a868c
Kuang Lu <lukuang@g115.farber.hpc.udel.edu>
2016-10-12 10:59:38 -0400

    compare feautres in terms of feature weights

---
+++ sentence_level/test_effectiveness/compare_top_important_features.py
@@ -0,0 +1,86 @@
+"""
+compare top features accross instance in terms of feature weights
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+import codecs
+import cPickle
+
+def load_top_features(feature_data_dir,dir_name,feature_size):
+    top_features = {}
+    for instance in os.walk(feature_data_dir).next()[1]:
+        instance_feature_data_dir = os.path(feature_data_dir,instance,dir_name)
+        word_feature_file = os.path.join(instance_feature_data_dir,"word_feature")
+        classifier_file = os.path.join(instance_feature_data_dir,"clf")
+        
+        word_features = json.load(open(word_feature_file))
+        clf = cPickle.load(classifier_file)
+        coeff = map(abs,clf._coef[0])
+
+        lst = zip(word_features,coeff)
+        sorted_features = sorted(lst, key = lambda x:x[1])
+        top_features[instance] = []
+        for i in range(feature_size):
+            top_features.append(sorted_features[i][0])
+
+    return top_features
+
+
+def show_unique_top_features(top_features):
+    strict_unique = {}
+    unstrict_unique = {}
+    feature_counter = Counter()
+    feature_instance_map = {}
+    for instance in top_features:
+        strict_unique[instance] = []
+        unstrict_unique[instance] = []
+        for w in top_features[instance]:
+            feature_counter[w] += 1
+            if w not in feature_instance_map:
+                feature_instance_map[w] = []
+            feature_instance_map[w].append(instance)
+
+    
+    instance_size = len(top_features)
+    for w in feature_counter:
+        if feature_counter[w]!= instance_size:
+            if feature_counter[w] == 1:
+                instance_name = feature_instance_map[w][0]
+                strict_unique[instance_name].append(w)
+            else:
+                for instance_name in feature_instance_map[w]:
+                    unstrict_unique[instance_name].append(w)
+
+    print "Strict features:"
+    for instance in strict_unique:
+        print "%s:" %(instance)
+        for w in strict_unique[instance]:
+            print " %s" %(w)
+    
+    print '-'*20
+    print "Unstrict features:"
+    for instance in unstrict_unique:
+        print "%s:" %(instance)
+        for w in unstrict_unique[instance]:
+            print " %s" %(w)
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--feature_data_dir","-fd",default="/home/1546/code/keyword_extraction/sentence_level/data/eval/intra")
+    parser.add_argument("dir_name")
+    parser.add_argument("--feature_size","-fz",type=int,default=10)
+    args=parser.parse_args()
+
+
+    top_features = load_top_features(args.feature_data_dir,args.dir_name
+                                     args.feature_size)
+
+    show_unique_top_features(top_features)
+
+if __name__=="__main__":
+    main()
+
